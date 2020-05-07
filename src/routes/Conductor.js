db72363ce42a8303b24d01215f0aec089d092023
Jesusisimo <alarconcito_jr@hotmail.com>
2020-05-07 02:11:04 -0400

    probando rutas alternas

--- src/routes/Conductor.js
+++ src/routes/Conductor.js
@@ -12,7 +12,7 @@ class Conductor {
     config() {
         this.conductor.get('/', controller.conductorController.list);
         this.conductor.get('/:id', controller.conductorController.getOne);
-        this.conductor.post('/', auth, controller.conductorController.create);
+        this.conductor.post('/dump', auth, controller.conductorController.create);
         this.conductor.put('/:id', auth, controller.conductorController.update);
         this.conductor.delete('/:id', auth, controller.conductorController.delete);
     }
