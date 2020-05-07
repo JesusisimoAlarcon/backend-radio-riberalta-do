e18e8d43e271c23c1bc2884c28e5467501f981e7
Jesusisimo <alarconcito_jr@hotmail.com>
2020-05-06 23:10:24 -0400

    test cors en produccion

--- src/index.js
+++ src/index.js
@@ -26,9 +26,7 @@ class Servidor {
         //this.app.use(bodyparser.urlencoded({ extended: false }))
         //this.app.use(bodyparser.json())
         this.app.use(fileupload());
-        this.app.use(cors({
-            origin: 'http://localhost:3000'
-        }));
+        //this.app.use(cors({origin: 'http://localhost:3000'}));
     }
 
     variables_globales() {
