51bf8a392e717aa7e89b69b09e5ad4f3b1dbc315
Jesusisimo <alarconcito_jr@hotmail.com>
2020-05-08 00:04:06 -0400

    actualizacion de cors para el acceso de la app radio a la api del backend

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ class Servidor {
         //this.app.use(bodyparser.json())
         this.app.use(fileupload());
         this.app.use(cors({
-            origin: ['https://admin.radioriberalta.com.bo', 'http://localhost:3000']
+            origin: ['https://admin.radioriberalta.com.bo', 'http://localhost:3000', 'htts://www.radioriberalta.com.bo','http://radioriberalta.com.bo']
 /*
                 function (origin, callback) {
                     if (['https://admin.radioriberalta.com.bo', 'http://localhost:3000'].indexOf(origin) !== -1 || !origin) {
