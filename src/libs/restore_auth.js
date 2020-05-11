347dd8e1eaea0cc33613a0a91d1339511902fa29
Jesusisimo <alarconcito_jr@hotmail.com>
2020-05-11 09:33:25 -0400

    actualizacion de los registros de las noticias 112

--- src/libs/restore_auth.js
+++ src/libs/restore_auth.js
@@ -2,6 +2,7 @@ const pool = require('../database');
 const bcrypt = require('bcryptjs');
 async function actualizar_contrasena(password, usuario) {
     const salt = await bcrypt.genSalt(10);
+    console.log(salt)
     usuario.password = await bcrypt.hash(password, salt);
     const resp = await pool.query('UPDATE usuario SET ? WHERE idusuario = ?', [usuario, usuario.idusuario]);
     return resp;
